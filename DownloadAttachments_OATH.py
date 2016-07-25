83f617a07a13a5eb05a095a8e811823e57f1d6e2
alonphermanconsulting <alonphermanconsulting@gmail.com>
2016-07-24 22:25:09 -0400

    initial oath2 without attachments

--- DownloadAttachments_OATH.py
+++ DownloadAttachments_OATH.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import httplib2
 import os
 import base64
-import email
+import ConfigParser
 
 from apiclient import discovery
 from apiclient import errors
@@ -21,11 +21,45 @@ except ImportError:
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+#SCOPES = 'https://mail.google.com/'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
-MESSAGE_ID_FILE = 'ReadMessageIds.txt'
-EXISTING_MESSAGE_IDS = list()
-NEW_MESSAGE_IDS = list()
+
+def main():
+
+    config = ConfigParser.RawConfigParser()
+    config.read('config.properties')
+    attachment_directory = config.get('general', 'attachment_directory')
+    if 'attachments' not in os.listdir(attachment_directory):
+        os.mkdir(attachment_directory + r'\attachments')
+
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    threads = service.users().threads().list(userId='me').execute().get('threads', [])
+    for thread in threads:
+        tdata = service.users().threads().get(userId='me', id=thread['id']).execute()
+        nmsgs = len(tdata['messages'])
+
+        if nmsgs > 0:
+            msg = tdata['messages'][0]['payload']
+            subject = ''
+            for header in msg['headers']:
+                if header['name'] == 'Subject':
+                    subject = header['value']
+                    break
+            if subject:
+                print('%s (%d msgs)' % (subject, nmsgs))
+
+    messages = ListMessagesWithLabel(service, 'me', 'INBOX')
+    if not messages:
+        print('No messages found.')
+    else:
+        for message in messages:
+            message_details = service.users().messages().get(userId='me', id=message['id']).execute()
+           # print('added message ', message_details)
+            SaveAttachments(service=service, user_id='me', store_dir=attachment_directory + r'\attachments', msg_id=message['id'])
 
 
 def get_credentials():
@@ -56,56 +90,6 @@ def get_credentials():
             credentials = tools.run_flow(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
-
-
-def SaveAttachments(service, user_id, msg_id, store_dir):
-    """Get and store attachment from Message with given id.
-
-    Args:
-        service: Authorized Gmail API service instance.
-        user_id: User's email address. The special value "me"
-        can be used to indicate the authenticated user.
-    msg_id: ID of Message containing attachment.
-    store_dir: The directory used to store attachments.
-    """
-    try:
-        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-        if message['payload']:
-            for part in message['payload']['parts']:
-                if part['filename']:
-                # print(part['filename'])
-                # att_id = part['body']['attachmentId']
-                # # print(att_id)
-                # raw = service.users().messages().attachments().get(userId=user_id, messageId=id, id=att_id).execute()
-                # file_data = base64.urlsafe_b64decode(raw['data'].encode('UTF-8'))
-                #
-                # path = os.path.join(store_dir, part['filename'])
-                # print(path)
-                # f = open(path, 'w')
-                # f.write(file_data)
-                # f.close()
-
-                #file_data = base64.urlsafe_b64decode(part['body']['data'].encode('UTF-8'))
-                #path = ''.join([store_dir, part['filename']])
-                #f = open(path, 'w')
-                #f.write(file_data)
-                #f.close()
-
-                    prefix = ""
-
-                    if 'data' in part['body']:
-                        data=part['body']['data']
-                    else:
-                        att_id=part['body']['attachmentId']
-                        att=service.users().messages().attachments().get(userId=user_id, messageId=msg_id,id=att_id).execute()
-                        data=att['data']
-                    file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
-                    path = prefix+part['filename']
-                    with open(path, 'w') as f:
-                        f.write(file_data)
-
-    except errors.HttpError, error:
-        print('An error occurred: ', error)
 
 
 def ListMessagesWithLabel(service, user_id, label_name):
@@ -139,106 +123,44 @@ def ListMessagesWithLabel(service, user_id, label_name):
         print('An error occurred: ', error)
 
 
-def ListLabels(service, user_id):
-    """Get a list all labels in the user's mailbox.
+def SaveAttachments(service, user_id, msg_id, store_dir):
+    """Get and store attachment from Message with given id.
 
     Args:
         service: Authorized Gmail API service instance.
         user_id: User's email address. The special value "me"
         can be used to indicate the authenticated user.
-
-        Returns:
-    A list all Labels in the user's mailbox.
-    """
-
-    try:
-        response = service.users().labels().list(userId=user_id).execute()
-        labels = response['labels']
-        for label in labels:
-            # print( "Label id: {1} - Label name: {2}".format(label['id'], label['name']) )
-            print(label)
-        return labels
-    except errors.HttpError, error:
-        print('An error occurred: ', error)
-
-
-def checkIfMessageAlreadyDownloaded(message, http):
-    """Gets the id of the message
-
-    Args:
-        message: the message from gmail
-
-    Returns:
-        True/False
-    """
-
-    return True
-
-
-def GetMessage(service, user_id, msg_id):
-    """Get a Message with given ID.
-
-    Args:
-        service: Authorized Gmail API service instance.
-        user_id: User's email address. The special value "me"
-        can be used to indicate the authenticated user.
-        msg_id: The ID of the Message required.
-
-    Returns:
-        A Message.
+        msg_id: ID of Message containing attachment.
+        store_dir: The directory used to store attachments.
     """
     try:
         message = service.users().messages().get(userId=user_id, id=msg_id).execute()
-        print('Message snippet: ', message['snippet'])
+        if message['payload']:
+            for header in message['payload']['headers']:
+                if header['name'] == 'Subject':
+                    subject = header['value'].replace(':','').replace('/','').replace('"','')
+                    break
+            for part in message['payload']['parts']:
+                    prefix = subject
+                    if 'data' in part['body']:
+                        data=part['body']['data']
+                    elif 'attachmentId' in part['body'] :
+                        att_id=part['body']['attachmentId']
+                        att=service.users().messages().attachments().get(userId=user_id, messageId=msg_id,id=att_id).execute()
+                        data=att['data']
+                    else:
+                        continue
+                    file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+                    if message['payload']['filename']:
+                        docname = message['payload']['filename']
+                    else:
+                        docname = subject+'(NO FILE ATTACHED)'
+                    path = os.path.join(store_dir, docname)
+                    with open(path, 'w') as f:
+                        f.write(file_data)
 
-        return message
     except errors.HttpError, error:
         print('An error occurred: ', error)
-
-
-def main():
-    """Shows basic usage of the Gmail API.
-
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
-
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    if not os.path.exists(MESSAGE_ID_FILE):
-        open(MESSAGE_ID_FILE, 'w').close()
-    try:
-        f = open(MESSAGE_ID_FILE, mode='r+')
-    except IOError as e:
-        print('unable to open message id file', e)
-    messages = ListMessagesWithLabel(service, 'me', 'INBOX')
-    if not messages:
-        print('No messages found.')
-    else:
-        for message in messages:
-            message_details = service.users().messages().get(userId='me', id=message['id']).execute()
-            print('added message ', message_details)
-            SaveAttachments(service=service, user_id='me', store_dir='.', msg_id=message['id'])
-            # print(message['labelIds'])
- #   with open(MESSAGE_ID_FILE, mode='a') as f:
- #       f.write("\n".join(NEW_MESSAGE_IDS))
-
-
-# test_msg_id = '153c980f25738942'
-#     home_dir = os.path.expanduser('~')
-#     test_dir = os.path.join(home_dir,'Desktop/test')
-#     results = GetAttachments(service, 'me', test_msg_id, test_dir)
 
 if __name__ == '__main__':
     main()
