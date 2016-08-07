92b2889b3d7f1ae88e392c5de42a3e35184719db
alonphermanconsulting <alonphermanconsulting@gmail.com>
2016-08-06 21:36:50 -0400

    initial working with regex on test file

--- DownloadAttachments_OATH.py
+++ DownloadAttachments_OATH.py
@@ -1,17 +1,22 @@
 from __future__ import print_function
+from oauth2client.service_account import ServiceAccountCredentials
+from xml.dom.minidom import parseString
+import xmlrpclib
 import os
 import base64
 import ConfigParser
 import oauth2client
 import httplib2
 import pprint
-#
+import json
+import re
+
 from zipfile import ZipFile
 from apiclient import discovery
 from apiclient import errors
 from oauth2client import client
 from oauth2client import tools
-#
+
 try:
     import argparse
 
@@ -25,6 +30,23 @@ SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 #SCOPES = 'https://mail.google.com/'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
+EMAIL_MESSAGE_DUMP_FILE = "emails.txt"
+
+def findJsonValue(somejson, key):
+    def val(node):
+        # Searches for the next Element Node containing Value
+        e = node.nextSibling
+        while e and e.nodeType != e.ELEMENT_NODE:
+            e = e.nextSibling
+        return (e.getElementsByTagName('string')[0].firstChild.nodeValue if e
+                else None)
+    # parse the JSON as XML
+    #foo_dom = parseString(somejson)
+    foo_dom = parseString(xmlrpclib.dumps(somejson))
+    # and then search all the name tags which are P1's
+    # and use the val user function to get the value
+    return [val(node) for node in foo_dom.getElementsByTagName('name')
+            if node.firstChild.nodeValue in key]
 
 def main():
 
@@ -38,31 +60,58 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    # threads = service.users().threads().list(userId='me').execute().get('threads', [])
-    # for thread in threads:
-    #     tdata = service.users().threads().get(userId='me', id=thread['id']).execute()
-    #     nmsgs = len(tdata['messages'])
-    #     for message in tdata['messages']:
-    #         msg = message['payload']
-    #         subject = ''
-    #         print('payload ', msg['filename'])
-    #         for header in msg['headers']:
-    #             if header['name'] == 'Subject':
-    #                 subject = header['value']
-    #                 break
-    #         if subject:
-    #             print('%s (%d msgs)' % (subject, nmsgs))
+    # messages = ListMessagesWithLabel(service, 'me', 'INBOX')
+    # if not messages:
+    #     print('No messages found.')
+    # else:
+    #     for message in messages:
+    #         message_details = service.users().messages().get(userId='me', id=message['id']).execute()
 
-    messages = ListMessagesWithLabel(service, 'me', 'INBOX')
-    if not messages:
-        print('No messages found.')
-    else:
-        for message in messages:
-            message_details = service.users().messages().get(userId='me', id=message['id']).execute()
-            print('added message ', message_details)
-            pp = pprint.PrettyPrinter(indent=4)
-            pp.pprint(message_details)
-            SaveAttachments(service=service, user_id='me', store_dir=attachment_directory, msg_id=message['id'])
+    #with open(EMAIL_MESSAGE_DUMP_FILE, 'wt') as out:
+     #   res = json.dump(message_details, out, sort_keys=True, indent=4, separators=(',', ': '))
+
+        # TOCHANGE
+        # emailLines = open(EMAIL_MESSAGE_DUMP_FILE, "r")
+
+    # find messageid
+    emailLines = open("scratchpad.txt", "r")
+    for line in emailLines:
+        if re.match("(.*)id'(.*)", line):
+            strippedLine = line.replace(' ', '').replace('\'id\':', '').replace(':uu', '').replace('\'', ''). \
+                replace(',', '').replace('\n', '').replace('uu', '')
+            messageId = strippedLine
+
+    emailLines = open("scratchpad.txt", "r")
+
+    fileName = ''
+    attachmentId = ''
+    for line in emailLines:
+        if re.match("(.*)attachmentId(.*)", line):
+            strippedLine = line.replace(' ', '').replace('\'attachmentId\':', '').replace('{', ''). \
+                replace(',', '').replace('\n', '').replace('\'', '').replace('ubody', '').replace(':uu', '')
+            attachmentId = strippedLine
+        if re.match("(.*)filename(.*)", line) and not re.match("(.*)value(.*)", line) and not re.match("(.*)''(.*)",line):
+            strippedLine = line.replace(' ', '').replace('\'filename\':', ''). \
+                replace(',', '').replace('\n', '').replace('\'', '').replace('uu', '')
+            fileName = strippedLine
+        if fileName and attachmentId:
+            att = service.users().messages().attachments().get(userId='me', messageId=messageId,id=attachmentId).execute()
+            data = att['data']
+            file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+            path = os.path.join(attachment_directory, fileName)
+            if not os.path.isfile(path):
+                with open(path, 'w') as f:
+                    f.write(file_data)
+                    # if path.endswith(".zip"):
+                    #     zippedFile = ZipFile(path)
+                    #     zippedFile.extractall(store_dir)
+                    #     os.remove(path)
+                    continue
+
+          #  test = findJsonValue(message_details, 'attachmentId')
+          #  pp = pprint.PrettyPrinter(indent=4)
+          #  pp.pprint(message_details)
+          #  SaveAttachments(service=service, user_id='me', store_dir=attachment_directory, msg_id=message['id'])
 
 
 def get_credentials():
@@ -75,6 +124,14 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
+
+    # home_dir = os.path.expanduser('~')
+    # credential_dir = os.path.join(home_dir,
+    #                                'Google Drive/G/IT/Development/ETL/.credentials')  # JLD - customized path to working directory
+    # credential_path = os.path.join(credential_dir, 'client_secret.json')
+    # credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_path, scopes=SCOPES)
+
+
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir,
                                   'Google Drive/G/IT/Development/ETL/.credentials')  # JLD - customized path to working directory
@@ -139,12 +196,14 @@ def SaveAttachments(service, user_id, msg_id, store_dir):
     try:
         message = service.users().messages().get(userId=user_id, id=msg_id).execute()
         if message['payload']:
-            for header in message['payload']['headers']:
-                if header['name'] == 'Subject':
-                    subject = header['value'].replace(':','').replace('/','').replace('"','')
-                    break
+            # for header in message['payload']['headers']:
+            #     if header['name'] == 'Subject':
+            #         subject = header['value'].replace(':','').replace('/','').replace('"','')
+            #         break
             if 'parts' in message['payload']:
-                for part in message['payload']['parts']:
+                partsparent = message['payload']
+                parts = partsparent['parts']
+                for part in partsparent[parts]:
                     if part['filename']:
                         file_data = base64.urlsafe_b64decode(part['body']['data'].encode('UTF-8'))
                         path = ''.join([store_dir, part['filename']])
